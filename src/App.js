76266dceec398abb9abe836d8466047f904d7121
pandiji <skm808478@gmail.com>
2022-12-17 18:10:42 +0530

    kk1

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ import Calci from './component/Calci';
 
 function App(){
   return(
-          <>
+          <> 
+            <head>
+                <meta charset="UTF-8"/>
+                <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                {/* <title>Document</title> */}
+            </head> 
             <Header title="Calci Master"/>
             <Calci/>
           </>
